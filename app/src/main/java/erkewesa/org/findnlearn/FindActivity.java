f6b4000bd8bd721f51a1acfe2e1be17c0064abb6
3PNX <er.weiss@yahoo.de>
2018-12-20 20:18:12 +0100

    Find Activity geht noch nicht. Nullpointer Exception kommentiert

--- app/src/main/java/erkewesa/org/findnlearn/FindActivity.java
+++ app/src/main/java/erkewesa/org/findnlearn/FindActivity.java
@@ -1,46 +1,113 @@
 package erkewesa.org.findnlearn;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 
-import java.lang.reflect.Array;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 import java.util.ArrayList;
 
-import erkewesa.org.findnlearn.data.FindNLearnDbHelper;
-import erkewesa.org.findnlearn.data.StudiengangContract;
+
 
 public class FindActivity extends AppCompatActivity{
 
     Spinner studgSp;
+    Spinner semesterSp;
+    Spinner modulSp;
     Button btnGo;
-    private FindNLearnDbHelper myDbHelper;
-    private ArrayList<String> studiengaenge;
+
+    int anzSemester;
+    private DatabaseReference mDataBase;
+    private ArrayList<String> arrStudiengaenge =new ArrayList<String>();
+    private ArrayList<String> arrSemester=new ArrayList<String>();
+    private ArrayList<String> arrModule=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
+        mDataBase=FirebaseDatabase.getInstance().getReference();
+
         btnGo=findViewById(R.id.searchBtn);
         studgSp=findViewById(R.id.studgSp);
-        myDbHelper  = new FindNLearnDbHelper(this);
-        studiengaenge =new ArrayList<>();
-        studiengaenge =getStudiengangListe();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,studiengaenge);
-        studgSp.setAdapter(adapter);
-        btnGo.setOnClickListener(new View.OnClickListener() {
+        semesterSp=findViewById(R.id.semesterSp);
+        modulSp=findViewById(R.id.modulSp);
+
+        DatabaseReference dbStudiengänge=mDataBase.child("Studiengänge");
+        final ArrayAdapter<String> studAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrStudiengaenge);
+        final ArrayAdapter<String> semAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrSemester);
+        final ArrayAdapter<String> modulAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrModule);
+
+        studgSp.setAdapter(studAdapter);
+        semesterSp.setAdapter(semAdapter);
+        modulSp.setAdapter(modulAdapter);
+
+
+
+        dbStudiengänge.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                insertStudiengang();
-                displayDatabaseInfo();
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    String values=dataSnapshot.getValue(String.class);
+
+                    arrStudiengaenge.add(values);
+                    studAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        //NOCH NULLPOINTER EXCEPTION
+        String selectedStudiengang=studgSp.getSelectedItem().toString();
+        DatabaseReference dbSemester=mDataBase.child("Semester").child(selectedStudiengang);
+
+        dbSemester.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                anzSemester = dataSnapshot.getValue(Integer.class);
+                arrSemester.clear();
+
+                for(int i=1;i<=anzSemester;i++){
+                    arrSemester.add(""+i);
+                }
+                semAdapter.notifyDataSetChanged();
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
@@ -48,65 +115,18 @@ public class FindActivity extends AppCompatActivity{
 
 
 
-        displayDatabaseInfo();
+
+
+
 
     }
 
-    private ArrayList<String> getStudiengangListe(){
-        SQLiteDatabase db=myDbHelper.getReadableDatabase();
-        ArrayList<String> result= new ArrayList<String>();
-        Cursor cursor = db.rawQuery("SELECT "+StudiengangContract.StudiengangEntry.COLUMN_STUDIENGANG +
-                " FROM "+StudiengangContract.StudiengangEntry.TABLE_NAME,null);
-        try{
-            while(cursor.moveToNext()){
-                result.add(cursor.getString(0));
-            }
-        } finally {
-            cursor.close();
-        }
-
-        return result;
-    }
-
-    private void deleteTable(){
-        SQLiteDatabase db=myDbHelper.getWritableDatabase();
-
-        db.execSQL("Delete from "+StudiengangContract.StudiengangEntry.TABLE_NAME);
-    }
-
-    private void displayDatabaseInfo(){
 
 
 
-        SQLiteDatabase db=myDbHelper.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT * FROM "+ StudiengangContract.StudiengangEntry.TABLE_NAME,null);
-        try{
-            TextView displayView=(TextView) findViewById(R.id.dbTextView);
-            displayView.setText("Anzahl der Reihen in der Datenbank: " +cursor.getCount());
-        } finally {
-            cursor.close();
-        }
-    }
 
-    private void insertStudiengang(){
-        SQLiteDatabase db= myDbHelper.getWritableDatabase();
 
-        ContentValues values=new ContentValues();
-        values.put(StudiengangContract.StudiengangEntry.COLUMN_SEMESTER,2);
-        values.put(StudiengangContract.StudiengangEntry.COLUMN_STUDIENGANG,"Recht");
-        long newRowId=db.insert(StudiengangContract.StudiengangEntry.TABLE_NAME,null,values);
-
-        ContentValues values2=new ContentValues();
-        values2.put(StudiengangContract.StudiengangEntry.COLUMN_SEMESTER,1);
-        values2.put(StudiengangContract.StudiengangEntry.COLUMN_STUDIENGANG,"Mathematik 1");
-        long RowId=db.insert(StudiengangContract.StudiengangEntry.TABLE_NAME,null,values2);
-
-        ContentValues values3=new ContentValues();
-        values2.put(StudiengangContract.StudiengangEntry.COLUMN_SEMESTER,1);
-        values2.put(StudiengangContract.StudiengangEntry.COLUMN_STUDIENGANG,"Mobile Anwendungen");
-        long nRowId=db.insert(StudiengangContract.StudiengangEntry.TABLE_NAME,null,values3);
-    }
 
 
 
--- app/src/main/java/erkewesa/org/findnlearn/MainActivity.java
+++ app/src/main/java/erkewesa/org/findnlearn/MainActivity.java
@@ -5,13 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button buttonTest;
 
     private DatabaseReference mDatabase;
 
@@ -21,14 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonTest=findViewById(R.id.buttonTest);
         mDatabase=FirebaseDatabase.getInstance().getReference();
-        buttonTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mDatabase.child("Modul_01").child("Semester").setValue(1);
-            }
-        });
+
+
+
+
 
         //Buttons / ClickListener
         Button findBtn=findViewById(R.id.findBtn);
