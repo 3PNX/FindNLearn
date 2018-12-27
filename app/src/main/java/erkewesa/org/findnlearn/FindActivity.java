7c6a3e6cbea2f535b36930deef67a8ccd62e32f2
3PNX <er.weiss@yahoo.de>
2018-12-27 12:00:54 +0100

    GEHT

--- app/src/main/java/erkewesa/org/findnlearn/FindActivity.java
+++ app/src/main/java/erkewesa/org/findnlearn/FindActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -30,6 +32,9 @@ public class FindActivity extends AppCompatActivity{
     Spinner modulSp;
     Button btnGo;
 
+    String selectedStudiengang;
+    String selectedSemester;
+
     int anzSemester;
     private DatabaseReference mDataBase;
     private ArrayList<String> arrStudiengaenge =new ArrayList<String>();
@@ -43,7 +48,7 @@ public class FindActivity extends AppCompatActivity{
         mDataBase=FirebaseDatabase.getInstance().getReference();
 
         btnGo=findViewById(R.id.searchBtn);
-        studgSp=findViewById(R.id.studgSp);
+        studgSp= findViewById(R.id.studgSp);
         semesterSp=findViewById(R.id.semesterSp);
         modulSp=findViewById(R.id.modulSp);
 
@@ -51,6 +56,8 @@ public class FindActivity extends AppCompatActivity{
         final ArrayAdapter<String> studAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrStudiengaenge);
         final ArrayAdapter<String> semAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrSemester);
         final ArrayAdapter<String> modulAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrModule);
+
+
 
         studgSp.setAdapter(studAdapter);
         semesterSp.setAdapter(semAdapter);
@@ -65,6 +72,7 @@ public class FindActivity extends AppCompatActivity{
 
                     arrStudiengaenge.add(values);
                     studAdapter.notifyDataSetChanged();
+
             }
 
             @Override
@@ -87,27 +95,90 @@ public class FindActivity extends AppCompatActivity{
 
             }
         });
-        //NOCH NULLPOINTER EXCEPTION AB HIER
-        String selectedStudiengang=studgSp.getSelectedItem().toString();
-        DatabaseReference dbSemester=mDataBase.child("Semester").child(selectedStudiengang);
 
-        dbSemester.addValueEventListener(new ValueEventListener() {
+        studgSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                anzSemester = dataSnapshot.getValue(Integer.class);
-                arrSemester.clear();
-
-                for(int i=1;i<=anzSemester;i++){
-                    arrSemester.add(""+i);
-                }
-                semAdapter.notifyDataSetChanged();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedStudiengang=studgSp.getSelectedItem().toString();
+                studAdapter.notifyDataSetChanged();
+                semesterSp.setSelection(0);
 
 
+                DatabaseReference dbSemester=mDataBase.child("Semester").child(selectedStudiengang);
+
+
+                dbSemester.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        anzSemester = dataSnapshot.getValue(Integer.class);
+                        arrSemester.clear();
+
+                        for(int i=1;i<=anzSemester;i++){
+                            arrSemester.add(""+i);
+                        }
+                        semAdapter.notifyDataSetChanged();
+
+
+
+
+                    }
+
+                   @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                semesterSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        arrModule.clear();
+                        selectedSemester=semesterSp.getSelectedItem().toString();
+
+
+                        DatabaseReference dbModul=mDataBase.child("Module").child(selectedStudiengang).child("Semester "+selectedSemester);
+
+                        dbModul.addChildEventListener(new ChildEventListener() {
+                            @Override
+                            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                String values =dataSnapshot.getValue(String.class);
+                                arrModule.add(values);
+                                modulAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                            }
+
+                            @Override
+                            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                            }
+
+                            @Override
+                            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
 
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
@@ -115,19 +186,7 @@ public class FindActivity extends AppCompatActivity{
 
 
 
-
-
-
-
     }
-
-
-
-
-
-
-
-
 
 
 }
