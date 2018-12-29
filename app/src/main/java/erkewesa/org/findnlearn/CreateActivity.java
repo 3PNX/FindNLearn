4fdc6d6441edb9abce3aa787202b879a2802e160
3PNX <er.weiss@yahoo.de>
2018-12-29 17:57:28 +0100

    Create Activity angefangen, 27.12.

--- app/src/main/java/erkewesa/org/findnlearn/CreateActivity.java
+++ app/src/main/java/erkewesa/org/findnlearn/CreateActivity.java
@@ -1,44 +1,269 @@
 package erkewesa.org.findnlearn;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.text.method.DateTimeKeyListener;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import android.widget.ArrayAdapter;
 import java.util.Date;
 
 public class CreateActivity extends AppCompatActivity {
+    private DatabaseReference mRef;
 
     Spinner cr_stuga;
     Spinner cr_se;
     Spinner cr_mod;
-    Date cr_tag;
+    TextView cr_datum;
     Button cr_create;
 
-    private DatabaseReference mDataBase;
-    private ArrayList<String> arr_cr_stuga = new ArrayList<String>();
-    private ArrayList<String> arr_cr_se = new ArrayList<String>();
-    private ArrayList<String> arrModule = new ArrayList<String>();
+    // das Zeugs aus Find
+    String selectedStudiengang;
+    String selectedSemester;
 
+    TextView tvStudiengang;
+    TextView tvSemester;
+    TextView tvModul;
+
+    int anzSemester;
+    private DatabaseReference mDataBase;
+    private DatabaseReference dbSemester;
+    private DatabaseReference dbModul;
+    private ArrayList<String> arrStudiengaenge =new ArrayList<String>();
+    private ArrayList<String> arrSemester=new ArrayList<String>();
+    private ArrayList<String> arrModule=new ArrayList<String>();
+    // bis hier
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
-        mDataBase=FirebaseDatabase.getInstance().getReference();
+//        mDataBase=FirebaseDatabase.getInstance().getReference();
+        mRef = FirebaseDatabase.getInstance().getReference();
 
         cr_stuga= findViewById(R.id.cr_stuga);
         cr_se=findViewById(R.id.cr_se);
         cr_mod=findViewById(R.id.cr_mod);
-//        cr_tag=findViewById(R.id.cr_tag);
+        cr_datum= findViewById(R.id.cr_datum);
+        cr_create=(Button) findViewById(R.id.cr_create);
 
+        //find jo
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+
+        cr_stuga= findViewById(R.id.cr_stuga);
+        cr_se=findViewById(R.id.cr_se);
+        cr_mod=findViewById(R.id.cr_mod);
+//        tvStudiengang=findViewById(R.id.studgTxV);
+//        tvSemester=findViewById(R.id.semesterTxV);
+//        tvModul=findViewById(R.id.modulTxV);
+
+
+
+
+        DatabaseReference dbStudiengänge=mDataBase.child("Studiengänge");
+        final ArrayAdapter<String> studAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrStudiengaenge);
+        final ArrayAdapter<String> semAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrSemester);
+        final ArrayAdapter<String> modulAdapter = new ArrayAdapter<>(this,android.R.layout.simple_selectable_list_item, arrModule);
+
+        cr_stuga.setAdapter(studAdapter);
+        cr_se.setAdapter(semAdapter);
+        cr_mod.setAdapter(modulAdapter);
+
+        // noch immer Bums aus Find
+        dbStudiengänge.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String values=dataSnapshot.getValue(String.class);
+
+                arrStudiengaenge.add(values);
+                studAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        cr_stuga.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedStudiengang=cr_stuga.getSelectedItem().toString();
+                studAdapter.notifyDataSetChanged();
+                cr_se.setSelection(0);
+
+
+                try {
+                    dbSemester= mDataBase.child("Semester").child(selectedStudiengang);
+                    tvSemester.setVisibility(View.VISIBLE);
+                    cr_se.setVisibility(View.VISIBLE);
+                }catch(DatabaseException ex){
+                    dbSemester = mDataBase.child("Semester").child("Wahl");
+                    tvSemester.setVisibility(View.INVISIBLE);
+                    cr_se.setVisibility(View.INVISIBLE);
+                    tvModul.setVisibility(View.INVISIBLE);
+                    cr_mod.setVisibility(View.INVISIBLE);
+                }
+
+                dbSemester.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        try {
+                            anzSemester = dataSnapshot.getValue(Integer.class);
+                            arrSemester.clear();
+                            arrSemester.add("Bitte wählen...");
+
+                            for (int i = 1; i <= anzSemester; i++) {
+                                arrSemester.add("" + i);
+                            }
+                            semAdapter.notifyDataSetChanged();
+                        } catch(Exception e){
+                            String value =dataSnapshot.getValue(String.class);
+                            arrSemester.add(value);
+                            semAdapter.notifyDataSetChanged();
+
+                        }
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                cr_se.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        arrModule.clear();
+                        selectedSemester=cr_se.getItemAtPosition(position).toString();
+
+
+                        if(!selectedStudiengang.equals("Bitte wählen...")||!selectedSemester.equals("Bitte wählen...")) {
+                            try {
+                                dbModul = mDataBase.child("Module").child(selectedStudiengang).child("Semester " + selectedSemester);
+                                tvModul.setVisibility(View.VISIBLE);
+                                cr_mod.setVisibility(View.VISIBLE);
+
+
+                                dbModul.addChildEventListener(new ChildEventListener() {
+                                    @Override
+                                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                                        String values = dataSnapshot.getValue(String.class);
+                                        arrModule.add(values);
+                                        modulAdapter.notifyDataSetChanged();
+                                    }
+
+                                    @Override
+                                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                                    }
+
+                                    @Override
+                                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                                    }
+
+                                    @Override
+                                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }catch(DatabaseException e){
+                                tvModul.setVisibility(View.INVISIBLE);
+                                cr_mod.setVisibility(View.INVISIBLE);
+                                arrModule.clear();
+                                arrModule.add("Bitte wählen...");
+                                modulAdapter.notifyDataSetChanged();
+                            }
+                        } else{
+                            arrModule.add("Bitte wählen...");
+                            modulAdapter.notifyDataSetChanged();
+                        }
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+        // end of find jo
+
+        // meet erstellen
+        cr_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference mRefChild = mRef.child("meet");
+
+                Meetings m = new Meetings();
+                m.setDatum((String) cr_datum.getText());
+                m.setModul((String) cr_mod.getSelectedItem());
+                m.setSemester((Long) cr_se.getSelectedItem());
+                m.setStudiengang((String) cr_stuga.getSelectedItem());
+                m.setStudg_modul((String) cr_stuga.getSelectedItem() + "_" + cr_mod.getSelectedItem());
+                mRefChild.push().setValue(m);
+
+            }
+        });
+        //bis hier
     }
 }
