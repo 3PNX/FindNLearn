ec68eda0bdca76cbd951b0ce19581d733df879d9
3PNX <er.weiss@yahoo.de>
2019-01-04 17:36:31 +0100

    DesignVorschlag. Fast alle Screens sind designed. Kriege es beim SearchResult noch nicht hin mit der Textfarbe. Habs kurz in Weiß gehabt - das war gut - gehabt aber funktionierte danach - also nach weiterem Ausprobieren - nicht mehr.

--- app/src/main/java/erkewesa/org/findnlearn/ProfileActivity.java
+++ app/src/main/java/erkewesa/org/findnlearn/ProfileActivity.java
@@ -13,62 +13,12 @@ import com.google.firebase.database.DatabaseReference;
 public class ProfileActivity extends AppCompatActivity {
     private EditText pro_name, pro_stuga;
     private Button pro_save;
-    private User user;
-    private DatabaseReference mRef;
-    private DatabaseHelper dbh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //abfragen ob User schon in DB ist: Lokale DB vergleichen mit Firebase jo
-        // Verbindung zur loc DB aufbauen
 
-
-        // wenn das nicht der Fall ist, dann das...
-        pro_name = findViewById(R.id.pro_name);
-        pro_stuga = findViewById(R.id.pro_course);
-        pro_save = (Button) findViewById(R.id.pro_save);
-
-        pro_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //loc DB
-                    insertLocalData();
-                // end
-                // firebase
-                user = new User(pro_name.getText().toString(), pro_stuga.getText().toString());
-
-                try {
-                    DatabaseReference mRefChild = mRef.child("user");
-                    mRefChild.push().setValue(user);
-
-                }catch (DatabaseException e){
-                    e.printStackTrace();
-                }
-                //end
-
-
-
-            }
-        });
-
-        // wenn doch, dann Vergleich ab hier:
-
-
-
-        // bis hier
     }
-
-    private void insertLocalData (){
-        boolean result = dbh.insertData(pro_name.getText().toString(), pro_stuga.getText().toString());
-        if (result){
-            Toast.makeText(ProfileActivity.this, "Data inserted", Toast.LENGTH_LONG).show();
-        }else {
-            Toast.makeText(ProfileActivity.this, "Data insertion failed", Toast.LENGTH_LONG).show();
-        }
-    }
-
 }
